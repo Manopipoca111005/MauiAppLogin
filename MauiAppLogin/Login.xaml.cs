8ee7ccfb7951664c07db564caf1bc326c41ecd93
Manopipoca111005 <gustavoramos111005@gmail.com>
2025-04-24 17:03:03 +0100

    Finalização do projeto Login

--- MauiAppLogin/Login.xaml.cs
+++ MauiAppLogin/Login.xaml.cs
@@ -1,9 +1,51 @@
 namespace MauiAppLogin;
 
-public partial class NewPage1 : ContentPage
+public partial class Login : ContentPage
 {
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    async private void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            List<DadosUsuario> lista_usuarios = new List<DadosUsuario>
+            {
+                new DadosUsuario
+                {
+                    Usuario = "user1",
+                    Senha = "pass1"
+                },
+                new DadosUsuario
+                {
+                    Usuario = "user2",
+                    Senha = "pass2"
+                }
+            };
+
+            DadosUsuario dados_digitados = new DadosUsuario()
+            {
+                Usuario = txtUsuario.Text,
+                Senha = txtSenha.Text
+            };
+
+            if (lista_usuarios.Any(
+                i => dados_digitados.Usuario == i.Usuario &&
+                dados_digitados.Senha == i.Senha))
+            {
+                await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+                App.Current.MainPage = new Protegida();
+            }
+            else
+            {
+                throw new Exception("Usuário ou senha inválidos");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "Fechar");
+        }
+    }
 }
--- MauiAppLogin/Protegida.xaml.cs
+++ MauiAppLogin/Protegida.xaml.cs
@@ -5,5 +5,23 @@ public partial class Protegida : ContentPage
 	public Protegida()
 	{
 		InitializeComponent();
+
+		string? usuario_logado = null;
+
+		Task.Run(async () =>
+		{
+			usuario_logado = await SecureStorage.Default.GetAsync("usuario_logado");
+			lbl_boasvindas.Text = $"Bem-vindo(a) {usuario_logado}";
+        });
+	}
+
+	private async void Button_Clicked(object sender, EventArgs e)
+    {
+        bool confirmacao = await DisplayAlert("Quer mesmo sair?", "Sair do App?", "Sim", "Não");
+		if (confirmacao)
+		{
+			SecureStorage.Default.Remove("token");
+			App.Current.MainPage = new Login();
+        }
 	}
 }
